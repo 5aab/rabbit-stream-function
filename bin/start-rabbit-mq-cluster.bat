697eecd587c4a0660b932e5f45e3f69838e3b005
Ish Mahajan <ishmahajanbhanu@gmail.com>
2018-11-03 02:39:42 +0530

    bat file to start rabbit mq in cluster

---
+++ bin/start-rabbit-mq-cluster.bat
@@ -0,0 +1,42 @@
+c:
+
+cd C:\Program Files\RabbitMQ Server\rabbitmq_server-3.7.7\sbin
+
+
+set RABBITMQ_NODE_PORT=5672 
+set RABBITMQ_NODENAME=rabbit 
+start "Node-rabbit" rabbitmq-server -rabbitmq_management listener [{port,15672}]
+
+timeout /t 5
+
+set RABBITMQ_NODE_PORT=5673 
+set RABBITMQ_NODENAME=hare 
+start "Node-hare" rabbitmq-server -rabbitmq_management listener [{port,15673}]
+
+timeout /t 10
+
+set RABBITMQ_NODE_PORT=5674 
+set RABBITMQ_NODENAME=micky 
+start "Node-micky" rabbitmq-server -rabbitmq_management listener [{port,15674}]
+
+timeout /t 5
+
+set RABBITMQ_NODE_PORT=5675 
+set RABBITMQ_NODENAME=minni 
+start "Node-minni" rabbitmq-server -rabbitmq_management listener [{port,15675}]
+
+timeout /t 20
+
+call rabbitmqctl -n hare stop_app
+call rabbitmqctl -n hare join_cluster rabbit@DESKTOP-IO9KB8M
+call rabbitmqctl -n hare start_app
+
+call rabbitmqctl -n micky stop_app
+call rabbitmqctl -n micky join_cluster rabbit@DESKTOP-IO9KB8M
+call rabbitmqctl -n micky start_app
+
+call rabbitmqctl -n minni stop_app
+call rabbitmqctl -n minni join_cluster rabbit@DESKTOP-IO9KB8M
+call rabbitmqctl -n minni start_app
+
+timeout /t 10000
