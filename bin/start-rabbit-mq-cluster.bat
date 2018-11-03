5fae6d118823aec0f9be283963326db0fbb39ca5
Ish Mahajan <ishmahajanbhanu@gmail.com>
2018-11-03 13:58:50 +0530

    bat file to start rabbit mq in cluster

--- bin/start-rabbit-mq-cluster.bat
+++ bin/start-rabbit-mq-cluster.bat
@@ -6,6 +6,9 @@ cd C:\Program Files\RabbitMQ Server\rabbitmq_server-3.7.7\sbin
 set RABBITMQ_NODE_PORT=5672 
 set RABBITMQ_NODENAME=rabbit 
 start "Node-rabbit" rabbitmq-server -rabbitmq_management listener [{port,15672}]
+call rabbitmq-plugins enable rabbitmq_management
+call rabbitmqctl -n rabbit stop_app
+call rabbitmqctl -n rabbit start_app
 
 timeout /t 5
 
@@ -32,13 +35,13 @@ call rabbitmqctl -n hare join_cluster rabbit@DESKTOP-IO9KB8M
 call rabbitmqctl -n hare start_app
 
 call rabbitmqctl -n micky stop_app
-call rabbitmqctl -n micky join_cluster rabbit@DESKTOP-IO9KB8M set_policy ha-all "\.*" '{"ha-mode":"all"}'
+call rabbitmqctl -n micky join_cluster rabbit@DESKTOP-IO9KB8M
 call rabbitmqctl -n micky start_app
 
 call rabbitmqctl -n minni stop_app
-call rabbitmqctl -n minni join_cluster rabbit@DESKTOP-IO9KB8M set_policy ha-all "\.*" '{"ha-mode":"all"}'
+call rabbitmqctl -n minni join_cluster rabbit@DESKTOP-IO9KB8M
 call rabbitmqctl -n minni start_app
 
-call rabbitmqctl -n rabbit@DESKTOP-IO9KB8M set_policy "ha-failover" "\.*" "{\"ha-mode\":\"all\", \"ha-promote-on-shutdown\":\"always\", \"ha-promote-on-failure\":\"always\", \"queue-master-locator\":\"random\"}"
+call rabbitmqctl -n rabbit@DESKTOP-IO9KB8M set_policy "ha-failover" ".*" "{\"ha-mode\":\"all\", \"ha-promote-on-shutdown\":\"always\", \"ha-promote-on-failure\":\"always\", \"queue-master-locator\":\"random\", \"ha-sync-mode\":\"automatic\"}"
 
 timeout /t 10000
--- src/main/java/com/punjab/de/janwar/producer/Runner.java
+++ src/main/java/com/punjab/de/janwar/producer/Runner.java
@@ -5,12 +5,9 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.TimeUnit;
-
 @Component
 public class Runner implements CommandLineRunner {
 
-    private static final String topicExchangeName = "spring-boot-exchange";
     private final RabbitTemplate rabbitTemplate;
     private final Receiver receiver;
 
@@ -22,7 +19,5 @@ public class Runner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Sending message...");
-        rabbitTemplate.convertAndSend(topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
-        receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
     }
 }
